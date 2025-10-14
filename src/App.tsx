7992fe02ba9306a3fc4e541a20b4d813e31d20f0
Leonardo Duarte <leocc_etraud@outlook.com>
2025-10-13 22:25:09 -0300

    feat: acordando a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { ToastProvider } from "@heroui/react";
+import { useEffect } from "react";
 
 import { Router } from "./routes";
 import { AuthProvider } from "./contexts/AuthProvider";
 import { UserProvider } from "./contexts/UserProvider";
 import { ScheduleProvider } from "./contexts/ScheduleProvider";
+import { apiBarber } from "./services/apiServer";
 
 function App() {
+  useEffect(() => {
+    let cancelled = false;
+
+    (async () => {
+      try {
+        const { data } = await apiBarber.get("/healthz");
+
+        if (!cancelled) console.log("API online:", data);
+      } catch (err) {
+        if (!cancelled) console.error("Erro ao conectar API:", err);
+      }
+    })();
+
+    return () => {
+      cancelled = true;
+    };
+  }, []);
+
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | Barbearia" />
